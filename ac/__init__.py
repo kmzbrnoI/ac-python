b74ef5e0f5dcdc5f9a6993ffe4d571752c664697
Jan Horacek <me@apophis.cz>
2020-04-10 21:08:38 +0200

    Add __all__ to __init__.py.

--- ac/__init__.py
+++ ac/__init__.py
@@ -5,3 +5,8 @@ from .ac import ACs, AC, State
 from . import blocks
 from .blocks import Block
 from . import pt
+
+__all__ = [
+    init, on_connect, on_register, on_unregister, on_start, on_stop, on_pause,
+    on_resume, ACs, AC, State, blocks, Block, pt
+]
